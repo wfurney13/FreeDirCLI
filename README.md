fe9bb0e8c412218144f4c94e426b3f6ea2e46960
William Furney <wfurney13@gmail.com>
2024-01-07 15:23:44 -0600

    multiple changes

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
-Call the executable with a file path argument
+FreeDirCLI
 
-Example:
-.\FreeDirCLI.exe C:\
+![image](https://i.ibb.co/k0RM9f3/Screenshot-2024-01-07-152010.png)
 
-Or
+I use this tool for finding out which directories are using the most space since this info is not provided natively in the windows file explorer. This allows me to quickly determine which directories I may need to target for freeing up space. 
 
-.\FreeDirCLI.exe 'C:\Program Files (x86)\'
+There are two main ways to use this tool. First, you can run it with a 'file path' argument, such as C:\ or C:\Users. Alternatively you can run the tool with no arguments.
 
-This will get the names of the subdirectories at that path (only one level deep currently), add up their file sizes, and return the name and sum 
+As this tool is a CLI, it is designed to run from the command line. All of the below examples are using powershell.
 
-Build instructions (.net 7.0 required):
+Usage:
+-- clone the repo
+`git clone https://github.com/wfurney13/FreeDirCLI/`
+
+-- change directory to the 'bin' folder
+`cd .\FreeDirCLI\bin\`
+
+-- Run the program with a file path argument
+`.\fd.exe C:\`
+
+-- Add quotes if your file path contains a space
+`.\fd.exe 'C:\Program Files (x86)\'`
+
+This will get the names of the subdirectories at that path, add up their file sizes, and return the name and sum 
+
+When no file path is provided as an argument, this tool will scan all of your drives, sum up the file sizes of each directory, and return the results.
+
+
+-- Call the executable without a file path argument
+`.\fd.exe`
+
+
+There is also a lightmode switch ('-l'). For lightmode users or those who do not want colored text in the output, -l should be passed as the first argument.
+
+-- Call the executable without a file path argument and with the light mode switch
+`.\fd.exe -l`
+
+-- Call the executable with a file path argument and the light mode switch
+`.\fd.exe -l C:\`
+
+
+Optional Build instructions (.net 7.0 required):
 
 `git clone https://github.com/wfurney13/FreeDirCLI/`
 
-`cd .\FreeDirCLI\`
+`cd .\FreeDirCLI\src\`
 
 `dotnet build -c Release`
 
 FreeDirCLI.exe will be created at 
+`~\FreeDirCLI\src\bin\Release\net7.0\FreeDirCLI.exe`
 
-`~\FreeDirCLI\bin\Release\net7.0\FreeDirCLI.exe`
-
-I use this tool for finding out which directories are using the most space since this info is not provided natively in the windows file explorer.
+I rename the exe to 'fd.exe' and then add the file path to my PATH environment variable
---
+++ src/Program.cs
@@ -0,0 +1,129 @@
+﻿using System.ComponentModel;
+
+namespace FreeDirCLI;
+
+class Program
+{
+    public static bool prefersLightMode;
+
+    static void Main(string[] args)
+    {
+        SetPrefersLightMode(args);
+
+        if (args.Length == 0 || args[0] == "-l" && args.Length == 1)
+        {
+            GetSizeOfAllFolders();
+        }
+
+        if (args.Length == 1 && !prefersLightMode)
+        {
+            GetSizeOfEachFolderForPath(args[0]);
+        }
+
+        if (args.Length == 2 && prefersLightMode)
+        {
+            GetSizeOfEachFolderForPath(args[1]);
+        }
+    }
+
+    static void SetPrefersLightMode(string[] args)
+    {
+        if (args.Length > 0 && args[0] == "-l")
+        {
+            prefersLightMode = true;
+        }
+    }
+
+    static void Write(string message, ConsoleColor color, bool lightMode)
+    {
+        if (prefersLightMode == false)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        else //lightmode users do not get a fancy color
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    static void GetSizeOfAllFolders()
+    {
+        Write(
+            "When no file path is provided as an argument, this tool will scan all of your drives, sum up the file sizes of each directory, and return the results. Would you like to use the tool on all drives?",
+            ConsoleColor.Blue,
+            prefersLightMode
+        );
+        string? n = Console.ReadLine();
+        switch (n)
+        {
+            case "y":
+            case "Y":
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (var drive in drives)
+                {
+                    Write(
+                        $"\nRetreiving info for {drive}...\n",
+                        ConsoleColor.Blue,
+                        prefersLightMode
+                    );
+                    Write(
+                        $"Total Size: {Math.Round(drive.TotalSize / 1024d / 1024d / 1024d, 2)} GB\nFree Space: {Math.Round(drive.TotalFreeSpace / 1024d / 1024d / 1024d, 2)} GB\n",
+                        ConsoleColor.Blue,
+                        prefersLightMode
+                    );
+                    GetSizeOfEachFolderForPath(drive.ToString());
+                }
+                break;
+            default:
+                Write(
+                    "Run this program with a file path as an argument.",
+                    ConsoleColor.Blue,
+                    prefersLightMode
+                );
+                break;
+        }
+    }
+
+    static void GetSizeOfEachFolderForPath(string filePath)
+    {
+        //create instance of DirectoryInfo for file path provided
+        DirectoryInfo dirInfo = new(filePath);
+        //create a directories variable that is the enumerable directories of that class
+        IEnumerable<DirectoryInfo> directories = dirInfo.EnumerateDirectories();
+
+        Write(
+            $"\n{"Directory Name", -45}\tDirectory Size (GB)\n",
+            ConsoleColor.White,
+            prefersLightMode
+        );
+
+        //loop through each directory at the path
+
+        foreach (var dir in directories)
+        {
+            //try to get the dir size, if we dont have access to the dir just continue and let the user know
+            try
+            {
+                //for each dir, we need to enumerate all the files within, and store the sum of those file sizes in the dirSize variable
+                long dirSize = dir.EnumerateFiles("*", SearchOption.AllDirectories)
+                    .Sum(file => file.Length);
+
+                //byte to GB coversion
+                double GBDirSize = dirSize / 1024d / 1024d / 1024d;
+
+                //Write the name and file size sum of each directory to the console
+                Write(
+                    $"{dir.Name, -45}\t{Math.Round(GBDirSize, 4)}",
+                    ConsoleColor.Yellow,
+                    prefersLightMode
+                );
+            }
+            catch (System.UnauthorizedAccessException) // no access to the dir
+            {
+                Write($"No access to dir: {dir.Name}", ConsoleColor.Red, prefersLightMode);
+            }
+        }
+    }
+}
